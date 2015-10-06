fa278ca550ae3b5740155294a87f94fb581cd904
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2015-10-07 01:15:24 +0900

    refactored: moved inidef/porttype.go to device/porttype.go.
    InputPortType and its values are used in device package only.

--- device/porttype.go
+++ device/porttype.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package inidef
+package device
 
 // InputPortType defines port type used by gateway
 type InputPortType int
